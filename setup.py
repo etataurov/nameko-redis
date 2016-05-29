5da8df93bf53a952efa092596a1675dd0ba81a5c
Evgeny Tataurov <tatauroff@gmail.com>
2016-05-29 18:00:20 +0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='nameko-redis',
-    version='0.0.1',
-    url='https://github.com/etataurov/nameko-redis/tree/master',
+    version='1.0.0',
+    url='https://github.com/etataurov/nameko-redis/',
     license='Apache License, Version 2.0',
     author='etataurov',
     author_email='tatauroff@gmail.com',
@@ -12,5 +12,16 @@ setup(
         "nameko>=2.0.0",
         "redis"
     ],
-    description='Redis dependency for nameko services'
+    description='Redis dependency for nameko services',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
