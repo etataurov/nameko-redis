986c3155c8ec13c7c725c108a7f8c060c96d6cfb
Fabrizio Romano <gianchub@gmail.com>
2017-03-27 23:00:31 +0100

    Amend setup.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nameko-redis',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/etataurov/nameko-redis/',
     license='Apache License, Version 2.0',
     author='etataurov',
@@ -10,8 +10,14 @@ setup(
     py_modules=['nameko_redis'],
     install_requires=[
         "nameko>=2.0.0",
-        "redis"
+        "redis",
     ],
+    extras_require={
+        'test': [
+            "pytest>=3.0.7",
+            "mock>=2.0.0",
+        ],
+    },
     description='Redis dependency for nameko services',
     classifiers=[
         'Development Status :: 3 - Alpha',
